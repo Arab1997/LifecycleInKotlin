1a1b8022add8ca5fd2064899f8812f7bc2cd5c11
Admin <arab1997>
2020-08-12 11:47:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lifecycle/MyObserver.kt
@@ -0,0 +1,15 @@
+package com.example.lifecycle
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+import java.security.CodeSource
+
+class MyObserver : LifecycleObserver{
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    fun myFun(source: LifecycleOwner, event : Lifecycle.Event){
+        Log.d("TAG", event.toString())
+    }
+}
